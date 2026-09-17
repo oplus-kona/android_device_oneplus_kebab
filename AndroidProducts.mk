bacfdd1fdedfc79de4bba6bc4115e1c22433c7f0
zenzer0s <praveenzalaki200@gmail.com>
2026-09-17 06:39:23 +0000

    kebab: Add AOSPA product makefile and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aospa_kebab.mk \
     $(LOCAL_DIR)/lineage_kebab.mk
+
+COMMON_LUNCH_CHOICES := \
+    aospa_kebab-user \
+    aospa_kebab-userdebug \
+    aospa_kebab-eng
---
+++ aospa_kebab.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021-2024 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from kebab device
+$(call inherit-product, device/oneplus/kebab/device.mk)
+
+# Inherit from AOSPA common product configuration
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_NAME := aospa_kebab
+PRODUCT_DEVICE := kebab
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := KB2005
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="OnePlus8T-user 14 UKQ1.230924.001 R.1a46c89-1_1 release-keys" \
+    BuildFingerprint=OnePlus/OnePlus8T/OnePlus8T:14/UKQ1.230924.001/R.1a46c89-1_1:user/release-keys \
+    DeviceName=OnePlus8T \
+    DeviceProduct=OnePlus8T \
+    SystemDevice=OnePlus8T \
+    SystemName=OnePlus8T
